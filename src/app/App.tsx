164e770d6adaf27faf08f8a3b3d007da9058dbcb
Echora-1 <wonrut02@mail.ru>
2023-02-06 15:00:33 +0300

    add router config

---
+++ src/app/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './styles/index.scss';
+import {Link} from "react-router-dom";
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "./providers/ThemeProvider";
+import {AppRouter} from "app/providers/router";
+
+function App() {
+    const {theme, toggleTheme} = useTheme()
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>
+                TOGGLE THEME
+            </button>
+            <Link to={'/'} >Main</Link>
+            <Link to={'/about'}>About</Link>
+            <AppRouter />
+        </div>
+    );
+}
+
+export default App;
