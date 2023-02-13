6296720993fd78ad2295e2b7324f155428984cea
Kirill.Ivanov <wonrut02@mail.ru>
2023-02-13 18:32:09 +0300

    add errorBoundary

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -6,14 +6,19 @@ describe('classNames', () => {
     });
     test('with additional class', () => {
         const expected = 'someClass class1 class2';
-        expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected);
+        expect(classNames('someClass', {}, ['class1', 'class2']))
+            .toBe(expected);
     });
     test('with mods and additional class', () => {
+        const mods = { hovered: true, scrollable: false };
         const expected = 'someClass class1 class2 hovered';
-        expect(classNames('someClass', { hovered: true, scrollable: false }, ['class1', 'class2'])).toBe(expected);
+        expect(classNames('someClass', mods, ['class1', 'class2']))
+            .toBe(expected);
     });
     test('with mods undefined', () => {
+        const mods: any = { hovered: true, scrollable: undefined };
         const expected = 'someClass class1 class2 hovered';
-        expect(classNames('someClass', { hovered: true, scrollable: undefined }, ['class1', 'class2'])).toBe(expected);
+        expect(classNames('someClass', mods, ['class1', 'class2']))
+            .toBe(expected);
     });
 });
