277a97718cd929d56d32a08f192c2885aa6d7f6e
Kirill.Ivanov <wonrut02@mail.ru>
2023-02-16 20:26:44 +0300

    add checks

---
+++ src/shared/ui/Button/Button.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Button, ThemeButton } from './Button';
+
+export default {
+    title: 'shared/Button',
+    component: Button,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    children: 'Button',
+};
+
+export const Clear = Template.bind({});
+Clear.args = {
+    children: 'Button',
+    theme: ThemeButton.CLEAR,
+};
+
+export const ClearDark = Template.bind({});
+ClearDark.args = {
+    children: 'Button',
+    theme: ThemeButton.CLEAR,
+};
+ClearDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const Outline = Template.bind({});
+Outline.args = {
+    children: 'Button',
+    theme: ThemeButton.OUTLINE,
+};
